2114a6d2484369f97d2de2165166d80332cf9748
Dayna Cole <daynerco@gmail.com>
2024-02-28 19:51:54 -0500

    finished sql codes for query and analysis

--- analysis/employee_analysis.sql
+++ analysis/employee_analysis.sql
@@ -24,18 +24,26 @@ JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
 JOIN departments ON dept_emp.dept_no = departments.dept_no;
 
 -- 5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
-
-
-
+SELECT employees.first_name, employees.last_name,employees.sex
+FROM employees
+WHERE first_name = 'Hercules' and last_name LIKE 'B%';
 
 -- 6. List each employee in the Sales department, including their employee number, last name, and first name.
-
-
-
+SELECT employees.emp_no, employees.last_name, employees.first_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
 
 -- 7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
-
-
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Development' or departments.dept_name = 'Sales';
 
 -- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT employees.last_name, COUNT(last_name) AS "freq_count"
+FROM employees
+GROUP BY employees.last_name
+ORDER BY "freq_count" DESC;
